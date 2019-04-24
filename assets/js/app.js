d10ad8ff09a623a2a6137baafa8207e9314f3c03
JakedStevens <onegreentiger@gmail.com>
2019-04-24 02:46:05 -0500

    lots of html and css changes, jQuery Selectors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,46 @@
 $(document).ready(function() {
-    $("#category").hide(); //hide category page
+    $("#landing-window").show(); //show landing page
     $("#result-page").hide(); //results page hidden
+    $("#slider-page").hide();//slider page hidden
+    $("#hap-slider-status").hide();//hiding slider values
+    $("#pol-slider-status").hide();
+    $("#col-slider-status").hide();
 
 
 $("#start").on("click", function () {
-        $("#landing-window").hide();
-        $("#category").show();
+    $("#slider-page").show();
+    $("#landing-window").hide();
+    $("#result-page").hide();
         })
 
-// $("#happy").stepDown("input change", function (){
-// }) //decrease value
-
 $("#submit").on("click", function () {
+    $("#result-page").show();
     $("#landing-window").hide();
-    $("#category").hide();
-    $("#result-page").show(); //results displaying page
+    $("#slider-page").hide();
 })
 
 });
+
+//these three functions are reading the values of the sliders and storing them in a variable
+let polVal;
+let colVal;
+let hapVal;
+function polSliderChange(val) {
+    document.getElementById("pol-slider-status").innerHTML = val;
+    polVal = val;
+    console.log("Political Slider Value = ", polVal);
+}
+function hapSliderChange(val) {
+    document.getElementById("hap-slider-status").innerHTML = val;
+    hapVal = val;
+    console.log("Happiness Slider Value = ", hapVal);
+}
+function colSliderChange(val) {
+    document.getElementById("col-slider-status").innerHTML = val;
+    colVal = val;
+    console.log("Cost of Living Slider Value = ", colVal);
+}
+
+
+
 
